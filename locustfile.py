ae063acf8dd869f944d1da766d816ce59fbe9967
Mohammad Javad Nikbakht <javadnikbakht@mail.com>
2022-03-13 00:00:07 +0330

    another LoadTestShape sample added to locustfile

--- locustfile.py
+++ locustfile.py
@@ -78,3 +78,39 @@ class StepLoadShape(LoadTestShape):
             current_step = 10
         return (current_step * self.step_load, self.spawn_rate)
 
+
+# class StagesShape(LoadTestShape):       # This another way that we can define a loadtestshape for our test
+#     """
+#     A simply load test shape class that has different user and spawn_rate at
+#     different stages.
+#     Keyword arguments:
+#         stages -- A list of dicts, each representing a stage with the following keys:
+#             duration -- When this many seconds pass the test is advanced to the next stage
+#             users -- Total user count
+#             spawn_rate -- Number of users to start/stop per second
+#             stop -- A boolean that can stop that test at a specific stage
+#         stop_at_end -- Can be set to stop once all stages have run.
+#     """
+
+#     stages = [
+#         {"duration": 60, "users": 3, "spawn_rate": 0.05},
+#         {"duration": 60, "users": 6, "spawn_rate": 0.05},
+#         {"duration": 60, "users": 9, "spawn_rate": 0.05},
+#         {"duration": 60, "users": 12, "spawn_rate": 0.05},
+#         {"duration": 60, "users": 15, "spawn_rate": 0.05},
+#         {"duration": 60, "users": 18, "spawn_rate": 0.05},
+#         {"duration": 60, "users": 21, "spawn_rate": 0.05},
+#         {"duration": 60, "users": 24, "spawn_rate": 0.05},
+#         {"duration": 60, "users": 27, "spawn_rate": 0.05},
+#         {"duration": 300, "users": 30, "spawn_rate": 0.05},
+#     ]
+
+#     def tick(self):
+#         run_time = self.get_run_time()
+
+#         for stage in self.stages:
+#             if run_time < stage["duration"]:
+#                 tick_data = (stage["users"], stage["spawn_rate"])
+#                 return tick_data
+#         return None
+
